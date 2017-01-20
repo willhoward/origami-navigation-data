57818c534f3349f917b988f116efadb096325d51
Jake Champion <me@jakechampion.name>
2017-01-20 12:33:59 +0000

    Trim whitespace from end of lines (#9)
    * separate cli from build script
    
    * enable trimming spaces from end of lines in files being built

--- src/build.js
+++ src/build.js
@@ -3,9 +3,20 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
 const path = require('path');
+const trim = require('trim');
+
+function removeTrailingSpaces(file) {
+	const normalised = file.split('\n')
+		.map(line => trim.right(line))
+		.join('\n');
+	return normalised;
+}
 
 function readFiles(files) {
-	const strings = files.map(file => fs.readFileSync(file, 'utf8'));
+	const strings = files.map(file => {
+		const contents = fs.readFileSync(file, 'utf8');
+		return removeTrailingSpaces(contents);
+	});
 	return strings.join('\n');
 }
 
