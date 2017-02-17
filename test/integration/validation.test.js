b0af2d59b7d4ad8239231f00f2c195b2a790b214
Jake Champion <me@jakechampion.name>
2017-02-17 14:48:06 +0000

    Add tests to check all links are 200s	 (#16)
    * Add tests to check all links are 200s
    
    * make cli executable
    
    * stop the redirect from http to https from happenning
    
    * commerce.uk.reuters.com is not on https
    
    * remove two links which are not being used in the navigation

--- test/integration/validation.test.js
+++ test/integration/validation.test.js
@@ -1,21 +1,46 @@
 'use strict';
 
 const assert = require('proclaim');
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+const throat = require('throat')(10);
+const heads = require('heads');
+const url = require('url');
+
 
 describe('Navigation data', () => {
-	let v;
-	let d;
+	let schema;
+	let navigationData;
 
 	beforeEach(() => {
-		v = require('../../data/schema');
-		d = require('../../build/navigation');
+		schema = require('../../data/schema');
+		navigationData = require('../../build/navigation');
 	});
 
 	it('adheres to navigation schema', () => {
-		const valid = v.validate('root', d);
-		assert.isNull(v.errors, 'The navigation data did not match the scheme:' + JSON.stringify(v.errors, null, 4));
-		assert.equal(valid, true, 'The navigation data did not match the scheme' );
+		const valid = schema.validate('root', navigationData);
+		assert.isNull(schema.errors, 'The navigation data did not match the scheme:' + JSON.stringify(schema.errors, null, 4));
+		assert.equal(valid, true, 'The navigation data did not match the scheme');
+	});
+
+	describe('links', () => {
+
+		const linkYaml = yaml.safeLoad(
+			fs.readFileSync(
+				path.join(__dirname, '../../data/links.yaml'), 'utf8'
+			)
+		);
+
+		const urls = linkYaml.links.map(link => url.resolve('https://www.ft.com', link.url));
+
+
+		urls.map(url => {
+			throat(() => {
+				it(`${url} returns a 200 status code`, () => {
+					return heads(url).then(statusCode => assert.equal(statusCode, 200));
+				});
+			});
+		});
 	});
 });
-
-
